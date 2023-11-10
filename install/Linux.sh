2c75f9a938ff889f3b1ccfb5d84942816df51b47
Michael May <michaelpmay@live.com>
2023-11-10 14:37:55 -0700

    update

---
+++ install/Linux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z  $1 ]; then
+
+  echo "ERROR: provide a Python Version"
+  exit 1
+fi
+
+if [ -d "venv" ]; then
+  rm -r venv
+fi
+
+python$1 -m venv venv/
+source env/bin/activate
+python$1 -m pip install --upgrade pip
+pip install -r requirements.txt
