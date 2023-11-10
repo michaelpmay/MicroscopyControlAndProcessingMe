542a8563a13d80de4c67442c84eada752fe912a7
Michael May <michaelpmay@live.com>
2023-11-10 14:55:04 -0700

    update

--- install/Linux.sh
+++ install/Linux.sh
@@ -8,7 +8,11 @@ fi
 if [ -d "venv" ]; then
   rm -r venv
 fi
-
+apt update && sudo apt upgrade
+add-apt-repository ppa:deadsnakes/ppa -y
+apt update
+apt install python$1
+sudo apt install python$1
 python$1 -m venv venv/
 source venv/bin/activate
 python$1 -m pip install --upgrade pip
